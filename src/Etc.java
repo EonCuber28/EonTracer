9645238c0fd3ae1aa0f986eda5e562ee17ecbb5e
EonCuber28 <isaacacpersonal@gmail.com>
2025-07-24 03:17:29 -0600

    I got vectors to point out of the camera (hopefully).

---
+++ src/Etc.java
@@ -0,0 +1,12 @@
+public class Etc{
+    public double[] normaliseVector(double[] Vector){
+        double a2 = Vector[0]*Vector[0];
+        double b2 = Vector[1]*Vector[1];
+        double c2 = Vector[2]*Vector[2];
+        double length = Math.sqrt(a2+b2+c2);
+        return new double[]{
+                Vector[0]/length,
+                Vector[1]/length,
+                Vector[2]/length};
+    }
+}
---
+++ src/Funny.java
@@ -0,0 +1,6 @@
+// nothing is here. yet.
+// go ahead and fuck around to find out.
+
+public class Funny{
+
+}
